ccfdb298cd8fa370db3524583e150ac46e4f5413
Ryan Haycock <ryanhaycock@mail.weber.edu>
2020-03-23 21:51:00 -0600

    Final Submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,61 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+
+import "bootstrap"
+
+
+
+const displayRandombox = function() 
+{
+    let r = Math.random()
+    if (r > .5) 
+        {
+        document.write('<div class="red box"></div>')
+        }
+
+    else
+        {
+        document.write('<div class="blue box"></div>')
+        }
+}
+
+function displayNBoxesUsingFor(n) 
+{
+    for (let i = 0; i < n; i++)
+    {
+      displayRandombox()
+    }
+}
+
+function displayNBoxesUsingWhile(n)
+{
+    let j = 0
+
+while (j < n)
+{
+    displayRandombox()
+    j++
+}
+}
+
+function displayNBoxesUsingDO(n)
+{
+    let k = 0
+
+    do {
+        displayRandombox()
+        k++
+    } while (k < n)
+}
+
+displayNBoxesUsingFor(10)
+displayNBoxesUsingWhile(10)
+displayNBoxesUsingDO(10)
+
+const square = function(x)
+{
+    return x * x
+}
+
+//lert(square(10))
